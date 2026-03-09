ada17de463af780b5933cb24912a23e8952fcecc
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-03-09 19:36:32 +0200

    chore: add hello world

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
+    /// <summary>
+    /// This is the first program in C# that prints "Hello, World!" to the console.
+    /// It serves as a simple introduction to C# programming and demonstrates how to write a basic console application.
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine();
         }
     }
 }
