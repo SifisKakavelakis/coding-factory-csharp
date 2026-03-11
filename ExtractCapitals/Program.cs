3cbed600f65735ebedc3766159896bb908a37f47
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-03-11 20:17:08 +0200

    chore: add palindrome app for strings manipulation

---
+++ ExtractCapitals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace ExtractCapitals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // Extract capital letters from a string
+        public static string ExtractCapitals(string input)
+        {
+            StringBuilder capitals = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (char.IsUpper(c))
+                {
+                    capitals.Append(c);
+                }
+            }
+            return capitals.ToString();
+        }
+    }
+}
