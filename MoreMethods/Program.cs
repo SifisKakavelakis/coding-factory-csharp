1611d763088a79d8ae96283ad4a0821e32abf9a4
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-03-11 20:59:31 +0200

    chore: add more functions

---
+++ MoreMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MoreMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            result = Add(1, 2, 3);  // 6
+            DateTime dateTime = getDateTime();
+        }
+
+        // Its is like overloading the method.
+        public static int Add(params int[] arr)
+        {
+            int sum = 0;
+
+            foreach (int num in arr)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
+        public static DateTime getDateTime(int year = 1970, int month = 1, int day = 1,
+            int min = 0, int sec = 0, int ms = 0)
+        {
+            return new DateTime(year, month, day, min, sec, ms);
+        }
+
+
+        // Overloading methods: same method name but different parameters.|
+        public static void SayHello(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void SayHello ()
+        {
+            Console.WriteLine("Coding");
+        }
+    }
+}
